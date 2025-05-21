02cb17dcad4ecaf49127163ecc823f4fc028af7d
Elchin Ismailov <ismailowwork@gmail.com>
2025-05-21 10:26:04 +0400

    feat: set up the Intro screen

--- app/src/main/java/com/example/projeman/IntroActivity.kt
+++ app/src/main/java/com/example/projeman/IntroActivity.kt
@@ -1,24 +1,20 @@
 package com.example.projeman
 
-import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.projeman.databinding.ActivitySplashBinding
+import com.example.projeman.databinding.ActivityIntroBinding
 
-class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -26,16 +22,8 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView) ?: return
         windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
         windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
-
-        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
-        binding.tvAppName.typeface = typeFace
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 2500)
     }
 }
