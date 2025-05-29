21772ac6f87367c740130dd8d30aca0e09a99f20
Elchin Ismailov <ismailowwork@gmail.com>
2025-05-29 11:17:39 +0400

    feat: register the user

--- app/src/main/java/com/example/projeman/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projeman/activities/SignUpActivity.kt
@@ -11,6 +11,8 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projeman.R
 import com.example.projeman.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : BaseActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -63,7 +65,20 @@ class SignUpActivity : BaseActivity() {
         val password: String = binding.etPassword.text.toString().trim()
 
         if(validateForm(name, email, password)) {
-            Toast.makeText(this@SignUpActivity, "Now we can register a new user", Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                task ->
+                hideProgressDialog()
+                if(task.isSuccessful) {
+                    val firebaseUser : FirebaseUser = task.result!!.user!!
+                    val registeredEmail = firebaseUser.email!!
+                    Toast.makeText(this, "$name you have successfully registered the email address $registeredEmail", Toast.LENGTH_LONG).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+                } else {
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
@@ -80,7 +95,8 @@ class SignUpActivity : BaseActivity() {
             TextUtils.isEmpty(password) -> {
                 showErrorSnackBar("Please enter a password")
                 false
-            } else -> {
+            }
+            else -> {
                 true
             }
         }
