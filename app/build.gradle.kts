fcfd7acb6c7f5c1f04f916e12f6be1c03f66e2b4
Elchin Ismailov <ismailowwork@gmail.com>
2025-06-10 09:59:27 +0400

    feat: prepare XML for the drawer and navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+
+    implementation (libs.circleimageview)
 }
