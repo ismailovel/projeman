1cb3d67a4c90669d1b47c57eb99fa380d534f5d8
Elchin Ismailov <ismailowwork@gmail.com>
2025-06-25 15:09:41 +0400

    feat: create BoardItemsAdapter

---
+++ app/src/main/java/com/example/projeman/adapters/BoardItemsAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.projeman.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projeman.R
+import com.example.projeman.databinding.ItemBoardBinding
+import com.example.projeman.models.Board
+
+open class BoardItemsAdapter(
+    private val context: Context,
+    private var list: ArrayList<Board>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var onClickListener: OnClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = ItemBoardBinding.inflate(LayoutInflater.from(context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if (holder is MyViewHolder) {
+            Glide
+                .with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_board_place_holder)
+                .into(holder.binding.ivBoardImage)
+
+            holder.binding.tvName.text = model.name
+            holder.binding.tvCreatedBy.text = "Created By : ${model.createdBy}"
+
+            holder.itemView.setOnClickListener {
+                if (onClickListener != null) {
+                    onClickListener!!.onClick(position, model)
+                }
+            }
+        }
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, model: Board)
+    }
+
+    private class MyViewHolder(val binding: ItemBoardBinding) : RecyclerView.ViewHolder(binding.root)
+}
