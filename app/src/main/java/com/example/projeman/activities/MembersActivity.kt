6537690a6e9b724a1fe3c7cd435adc7e30c0960e
Elchin Ismailov <ismailowwork@gmail.com>
2025-07-21 11:19:05 +0400

    feat: create the Member Item and the Toolbar

--- app/src/main/java/com/example/projeman/activities/MembersActivity.kt
+++ app/src/main/java/com/example/projeman/activities/MembersActivity.kt
@@ -3,19 +3,46 @@ package com.example.projeman.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projeman.R
+import com.example.projeman.databinding.ActivityMembersBinding
+import com.example.projeman.models.Board
+import com.example.projeman.utils.Constants
 
 class MembersActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMembersBinding
+    private lateinit var mBoardDetails: Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_members)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMembersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarMembersActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.members)
+        }
+
+        binding.toolbarMembersActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
