c01e0ab562e50416ae168a7f7ea731ab5d8d3079
Elchin Ismailov <ismailowwork@gmail.com>
2025-07-25 10:56:43 +0400

    feat: load card details to set the title of the card

--- app/src/main/java/com/example/projeman/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projeman/activities/CardDetailsActivity.kt
@@ -6,16 +6,54 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projeman.R
+import com.example.projeman.databinding.ActivityCardDetailsBinding
+import com.example.projeman.models.Board
+import com.example.projeman.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardDetailsBinding
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_card_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityCardDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        getIntentData()
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarCardDetailsActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+
+        binding.toolbarCardDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
         }
     }
 }
